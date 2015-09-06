4126802b2be714da9d4ce36b33cd31b7fc70d167
Collin RM Stocks <collinstocks@gmail.com>
2015-09-06 17:14:50 -0400

    bug fixes in class.js

--- src/lib/class.js
+++ src/lib/class.js
@@ -21,12 +21,13 @@ exports.Class = (function (Object, WeakMap) {
             var f = {};
             var priv = Object.create(f);
             var parents = new WeakMap;
-            var iinfo = instance_info[f] = {a: ancestors, p: parents, h: priv};
+            var iinfo = {a: ancestors, p: parents, h: priv};
+            instance_info.set(f, iinfo);
 
             for (var i = 0; i < Parents.length; ++i) {
                 var Parent = Parents[i]
                 var parent = new Parent(magic);
-                parents[Parent] = parent;
+                parents.set(Parent, parent);
                 Object.assign(f, parent);
             }
 
@@ -44,10 +45,12 @@ exports.Class = (function (Object, WeakMap) {
 
             if (m !== magic)
                 F.init(f, arguments);
+
+            return Object.freeze(f);
         };
 
         F.init = function (f, args) {
-            var iinfo = instance_info[f];
+            var iinfo = instance_info.get(f);
             // If f has already been initialized, throw.
             if (iinfo.i) throw new Error('already initialized');
             iinfo.i = 1;
@@ -57,21 +60,22 @@ exports.Class = (function (Object, WeakMap) {
             // For each parent:
             for (var i = 0; i < Parents.length; ++i) {
                 // Get the parent object.
-                var parent = iinfo.p[Parents[i]];
+                var parent = iinfo.p.get(Parents[i]);
                 // Initialize the parent object if not already initialized.
-                if (!instance_info[parent].i)
+                if (!instance_info.get(parent).i)
                     Parents[i].init(parent, []);
             }
         };
 
-        var cinfo = class_info[F] = {I: id_count++};
+        var cinfo = {I: id_count++};
+        class_info.set(F, cinfo);
         var ancestors = cinfo.a = {};
         ancestors[cinfo.i] = 1;
         for (var i = 0; i < Parents.length; ++i) {
             var Parent = Parents[i];
             if (!class_info.has(Parent))
                 throw new Error('parent '+i+' not a class');
-            Object.assign(ancestors, class_info[Parent].a);
+            Object.assign(ancestors, class_info.get(Parent).a);
         }
 
         return Object.freeze(F);
@@ -80,13 +84,13 @@ exports.Class = (function (Object, WeakMap) {
     Class.issubclass = function (A, B) {
         return !!(class_info.has(A)
             && class_info.has(B)
-            && class_info[A].a[class_info[B].I]);
+            && class_info.get(A).a[class_info.get(B).I]);
     };
 
     Class.isinstance = function (a, B) {
         return !!(instance_info.has(a)
             && class_info.has(B)
-            && instance_info[a].a[class_info[B].I]);
+            && instance_info.get(a).a[class_info.get(B).I]);
     };
 
     return Object.freeze(Class);
