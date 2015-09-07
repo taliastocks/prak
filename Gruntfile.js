2b95ea5f95a1514a759e84b30cecea3955dde539
Collin RM Stocks <collinstocks@gmail.com>
2015-09-06 22:33:02 -0400

    start writing parser generator (will probably switch to something automatically generated)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,55 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        clean: ['build/'],
+        jison: {
+            parser: {
+                options: {
+
+                },
+                files: [{
+                    src: 'src/grammar.jison',
+                    dest: 'generated/parser.js'
+                }]
+            }
+        },
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                        '<%= grunt.template.today("yyyy-mm-dd") %> */\n"use strict";'
-            },
             build: {
+                options: {
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                            '<%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+                            '"use strict";'
+                },
                 files: [{
                   expand: true,
                   cwd: 'src/',
                   src: '**/*.js',
                   dest: 'build/'
-              }]
-          },
-          bin: {
-              options: {
-                  banner: '#!/usr/bin/env node\n/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                          '<%= grunt.template.today("yyyy-mm-dd") %> */\n"use strict";'
-              },
-              files: [{
-                  src: 'src/bin-prak',
-                  dest: 'build/bin-prak'
-              }]
-          }
-      },
-      clean: ['build/']
+              }, {
+                expand: true,
+                cwd: 'generated/',
+                src: '**/*.js',
+                dest: 'build/'
+            }]
+            },
+            bin: {
+                options: {
+                    banner: '#!/usr/bin/env node\n' +
+                            '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                            '<%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+                            '"use strict";'
+                },
+                files: [{
+                    src: 'src/bin-prak',
+                    dest: 'build/bin-prak'
+                }]
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-jison');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['jison', 'uglify']);
 };
