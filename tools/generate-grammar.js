b6e4e459ac44019a9f7733e3c6db06ee42a96a3a
Collin RM Stocks <collinstocks@gmail.com>
2015-09-09 18:50:56 -0400

    start bnf

--- tools/generate-grammar.js
+++ tools/generate-grammar.js
@@ -1,4 +1,4 @@
-var Parser = require('jison').Parser;
+var Generator = require('jison').Generator;
 
 var grammar = {
     'lex': {
@@ -30,6 +30,7 @@ var operator_prec = [
     '<< >>',
     '+ -',
     '* / %',
+    '.* ->*',
     'UPREFIX',
     'USUFFIX',
     '::'
@@ -61,7 +62,10 @@ grammar.lex.rules.push(['[0-9]+("."[0-9]*)?([eE][0-9]+)?\\b', 'return "NUMBER";'
 grammar.lex.rules.push(['"."[0-9]+([eE][0-9]+)?\\b', 'return "NUMBER";']);
 grammar.lex.rules.push(['"0x"[0-9A-Fa-f]+\\b', 'return "NUMBER";']);
 grammar.lex.rules.push(['[A-Za-z_][0-9A-Za-z_]*', 'return "IDENTIFIER";']);
+grammar.lex.rules.push(['<<EOF>>', 'return "EOF";']);
 
+grammar.start = 'start';
+grammar.bnf.start = [['statements EOF', 'return $1;']];
 
 exports.output = function () {
     return 'TODO';
