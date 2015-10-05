f9e289ce55a7f46a02121fe47096d73ae132176c
Collin RM Stocks <collinstocks@gmail.com>
2015-10-04 23:39:41 -0400

    fix regex

--- tools/generate-grammar.js
+++ tools/generate-grammar.js
@@ -33,7 +33,7 @@ grammar.lex.rules.push(['[0-9]+(\\.[0-9]*)?([eE][0-9]+)?\\b', 'return "NUMBER";'
 grammar.lex.rules.push(['\\.[0-9]+([eE][0-9]+)?\\b', 'return "NUMBER";']);
 grammar.lex.rules.push(['0x[0-9A-Fa-f]+\\b', 'return "NUMBER";']);
 grammar.lex.rules.push(['[A-Za-z_][0-9A-Za-z_]*', 'return "IDENTIFIER";']);
-grammar.lex.rules.push(['<<EOF>>', 'return "EOF";']);
+grammar.lex.rules.push(['$', 'return "EOF";']);
 
 grammar.start = 'start';
 grammar.bnf.start = [
